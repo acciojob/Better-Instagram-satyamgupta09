0ce91ff4be30890cf3212ca648f50219f43d7bd9
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-03-04 03:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-//your code here
+// let draggedElement = null;
+
+// document.querySelectorAll(".image").forEach((div) => {
+//   div.addEventListener("dragstart", (e) => {
+//     draggedElement = e.target;
+//   });
+
+//   div.addEventListener("dragover", (e) => {
+//     e.preventDefault();
+//   });
+
+//   div.addEventListener("drop", (e) => {
+//     e.preventDefault();
+    
+//     // Simple swap logic
+//     if (draggedElement && draggedElement !== e.target) {
+//       let temp = document.createElement("div"); // Temporary placeholder
+//       e.target.replaceWith(temp);
+//       draggedElement.replaceWith(e.target);
+//       temp.replaceWith(draggedElement);
+//     }
+    
+//     draggedElement = null;
+//   });
+// });
+
+
+let dragElement=null;
+
+document.querySelectorAll(".image").forEach((div)=>{
+	div.addEventListener('dragstart', (e)=> {
+		dragElement = e.target;
+	})
+
+	div.addEventListener('dragover', (e)=> {
+		e.preventDefault();
+	})
+
+	div.addEventListener('drop', (e)=> {
+		e.preventDefault();
+		if(dragElement && dragElement !== e.target){
+			let temp = document.createElement("div");
+		    e.target.replaceWith(temp);
+		    dragElement.replaceWith(e.target);
+		    temp.replaceWith(dragElement);
+		}
+		dragElement=null;
+	})
+})
